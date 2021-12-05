76e2afaee1203638499ca4e7d26132c7e9adeac9
YauhenKavalchuk <yauhen.kavalchuk@gmail.com>
2021-12-05 15:45:10 +0300

    Update app layout

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -10,15 +10,15 @@ export const Counter = () => {
   const [ text, setText ] = useState('');
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(text.trim());
+    navigator.clipboard.writeText(text);
   };
 
   return (
     <>
       <header className={styles.header}>
-          <Count value={text} />
-          <CopyButton onClick={handleCopy} />
-        </header>
+        <Count value={text} />
+        <CopyButton onClick={handleCopy} />
+      </header>
       <section className={styles.section}>
         <Textarea value={text} onChange={setText} />
       </section>
